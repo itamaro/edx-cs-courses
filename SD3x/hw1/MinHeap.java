25e751e90c14f60695ce56472a0b36a504c5f811
Itamar Ostricher <itamarost@gmail.com>
2017-09-17 17:11:14 +0300

    SD3x HW1 solution

--- SD3x/hw1/MinHeap.java
+++ SD3x/hw1/MinHeap.java
@@ -2,12 +2,12 @@
 
 /**
  * A Heap implementation class
- * 
+ *
  * @param heap the array that holds the heap data
  * @param size the number of elements currently stored in the heap
  */
 public class MinHeap {
-	
+
 	CompareInt[] heap;
 	int size;
 
@@ -20,22 +20,59 @@ public class MinHeap {
 		heap = new CompareInt[n+1];
 		size = 0;
 	}
-	
+
 	/**
 	 * Adds an element to the heap
-	 * 
+	 *
 	 * @param val the value to be added to the heap
 	 */
 	public void add(CompareInt val) {
-		return;
+		if (size >= heap.length - 1) throw new IllegalArgumentException();
+		heap[++size] = val;
+		swim();
 	}
-	
+
 	/**
 	 * Extracts the smallest element from the heap
 	 */
 	public CompareInt extractMin() {
-		return null;
+		CompareInt ret = heap[1];
+		heap[1] = heap[size--];
+		sink();
+		return ret;
 	}
-	
-	
+
+	private void swim() {
+		int cur = size, parent = size / 2;
+		while (parent > 0) {
+			if (heap[parent].compareTo(heap[cur]) <= 0) return;
+			swap(parent, cur);
+			cur = parent;
+			parent = cur / 2;
+		}
+	}
+
+	private void sink() {
+		int cur = 1;
+		while (cur < size) {
+			int minChild = getMinChild(cur);
+			if (minChild == 0 || heap[cur].compareTo(heap[minChild]) <= 0) return;
+			swap(cur, minChild);
+			cur = minChild;
+		}
+	}
+
+	private void swap(int i, int j) {
+		CompareInt tmp = heap[i];
+		heap[i] = heap[j];
+		heap[j] = tmp;
+	}
+
+	private int getMinChild(int index) {
+		int left = 2 * index, right = 2 * index + 1;
+		if (left > size) return 0;
+		if (right > size) return left;
+		return (heap[left].compareTo(heap[right]) < 0 ? left : right);
+	}
+
 }
--- SD3x/hw1/Sorting.java
+++ SD3x/hw1/Sorting.java
@@ -1,27 +1,83 @@
 
 public class Sorting {
-	
+
 	/**
 	 * Implement the mergesort function, which should sort the array of
 	 * integers in place
-	 * 
+	 *
 	 * You will probably want to use helper functions here, as described in the lecture recordings
 	 * (ex. merge(), a helper mergesort function)
 	 * @param arr
 	 */
 	public static void mergeSort(CompareInt[] arr) {
-		//TODO
+		mergeSortHelper(arr, 0, arr.length - 1);
 	}
-	
+
+	private static void mergeSortHelper(CompareInt[] arr, int low, int high) {
+		if (low >= high) return;
+		int midPoint = (high + low) / 2;
+		mergeSortHelper(arr, low, midPoint);
+		mergeSortHelper(arr, midPoint + 1, high);
+		merge(arr, low, midPoint + 1, high);
+	}
+
+	private static void merge(CompareInt[] arr, int low, int midPoint, int high) {
+		int n = high - low + 1;
+		CompareInt[] tmp = new CompareInt[n];
+		System.arraycopy(arr, low, tmp, 0, n);
+		int left = 0, right = midPoint - low;
+		for (int i = low; i <= high;) {
+			if (tmp[left].compareTo(tmp[right]) < 0) {
+				arr[i++] = tmp[left++];
+				if (left >= midPoint - low) {
+					while (i <= high) arr[i++] = tmp[right++];
+				}
+			} else {
+				arr[i++] = tmp[right++];
+				if (right >= n) {
+					while (i <= high) arr[i++] = tmp[left++];
+				}
+			}
+		}
+	}
+
 	/**
 	 * Implement the quickSelect
-	 * 
+	 *
 	 * Again, you will probably want to use helper functions here
 	 * (ex. partition(), a helper quickselect function)
 	 */
 	public static CompareInt quickSelect(int k, CompareInt[] arr) {
-		//TODO
-		return null;
+		if (k > arr.length) return null;
+		return quickSelectHelper(k, arr, 0, arr.length - 1);
+	}
+
+	private static CompareInt quickSelectHelper(int k, CompareInt[] arr, int low, int high) {
+		int p = partition(arr, low, high);
+		if (k == p + 1) return arr[p];
+		if (k <= p) {
+			return quickSelectHelper(k, arr, low, p - 1);
+		}
+		return quickSelectHelper(k, arr, p + 1, high);
+	}
+
+	private static int partition(CompareInt arr[], int low, int high) {
+		CompareInt pivot = arr[high];  // always partition on last element
+		for (int i = low; i < high; i++) {
+			if (arr[i].compareTo(pivot) < 0) {
+				if (i != low)
+					swap(arr, i, low);
+				low++;
+			}
+		}
+		swap(arr, low, high);
+		return low;
+	}
+
+	private static void swap(CompareInt arr[], int i, int j) {
+		CompareInt tmp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = tmp;
 	}
 
 }
